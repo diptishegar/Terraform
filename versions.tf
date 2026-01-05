8f610efa4ccc4aa3a5ab83e9c57d44467a5bca3f
diptishegar <diptishegar6@gmail.com>
2026-01-05 17:05:31 +0530

    pipeline-run

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.14.0"
   backend "s3" {
     bucket         = "rapidconnect-terraform-state"
-    key            = "workspace.statefile"
+    key            = "terraform/workspace.statefile"
     region         = "ap-south-1"
     use_lockfile   = true 
     encrypt        = true
